164072a17ccedf4ad0cf96580c0463793dbd5080
Alex CHAVET <alex.chavet@gmail.com>
2019-11-03 19:16:09 +1100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="w-100 h-100">
+    <h1 class="grid" ref="title">Isabella</h1>
+  </section>
+</template>
+
+<script>
+import charming from "charming";
+export default {
+  mounted() {
+    this.$nextTick(() => {
+      const title = this.$refs.title;
+      charming(title, {
+        tagName: "span"
+      });
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
