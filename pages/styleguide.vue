3731ca449d8ce633d63ba41d5ec73b7c60401348
alex chavet <alex.chavet@gmail.com>
2019-11-14 11:20:11 +1100

    Create a styleguide

--- pages/styleguide.vue
+++ pages/styleguide.vue
@@ -4,6 +4,7 @@
       <h1>Heading one</h1>
       <h2>Heading two</h2>
       <h3>Heading three</h3>
+      <h4>Heading four</h4>
       <p>
         Paragraph with a <a href="#">link</a>
       </p>
