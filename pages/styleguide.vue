b7f19fc882b07533c7457d0e57afae95759d9c76
alex chavet <alex.chavet@gmail.com>
2019-11-14 11:15:34 +1100

    push styleguide

---
+++ pages/styleguide.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full mx-auto my-64">
+    <div class="wysiwyg w-full text-center">
+      <h1>Heading one</h1>
+      <h2>Heading two</h2>
+      <h3>Heading three</h3>
+      <p>
+        Paragraph with a <a href="#">link</a>
+      </p>
+    </div>
+  </div>
+</template>
